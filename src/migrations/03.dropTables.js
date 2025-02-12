5fe3b3c1578f7f73f979770964ecef2c98f8c974
Damien Chabanier <dam31vt@gmail.com>
2025-02-12 09:26:25 +0100

    bonne ecriture de l'import de sequelize

--- src/migrations/03.dropTables.js
+++ src/migrations/03.dropTables.js
@@ -1,4 +1,4 @@
-import sequelize from "../../data/connect.js";
+import sequelize from "../../data/sequelize.js";
 import { Card, List, Tag, CardHasTag } from "../models/index.js";
 async function dropTables() {
   await sequelize.drop({ cascade: true });
